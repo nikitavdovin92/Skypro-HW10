47fbffb0a03c2e17017149baa77b6f41761ee69f
Homework3 <tradervdovin@gmail.com>
2023-01-23 20:18:42 +0500

    HW10

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("задача 1");
+        System.out.println("task1");
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+
+
 
     }
-
 }
