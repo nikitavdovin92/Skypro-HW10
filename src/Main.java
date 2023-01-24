4d916404471faff371751f9b880372ad79dbc11f
Homework3 <tradervdovin@gmail.com>
2023-01-24 18:45:33 +0500

    HW10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("task1");
+        System.out.println("Задача 1");
+        task1();
+        System.out.println("Задача 2");
+        task2();
+        System.out.println("Задача 3");
+        task3();
+        System.out.println("Задача 4");
+        task4();
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
@@ -12,8 +21,47 @@ public class Main {
     }
     public static void task1() {
         int[] arr = generateRandomArray();
-
-
-
+        System.out.println(Arrays.toString(arr));
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int sum = 0;
+        int min = arr[0];
+        int max = arr [0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
+    }
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Cредняя сумма трат за месяц составила " + sum / arr.length + " рублей");
+    }
+    public static void task4() {
+        char [] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0 ; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+    }
+
+
+
 }
